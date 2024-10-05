132ae7eae0c013dea6e8d70073fb8e982bca1c64
Ishant Yadav <2020501189.ishant@ug.sharda.ac.in>
2024-10-05 18:30:44 +0530

    fifth commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { HomePageComponent } from "./home-page/home-page.component";
+import { LoginComponent } from "./login/login.component";
+import { RegisterComponent } from "./register/register.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomePageComponent],
+  imports: [HomePageComponent, LoginComponent, RegisterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -5,11 +5,12 @@ import { BenefitCardsComponent } from "./benefit-cards/benefit-cards.component";
 import { ExploreComponent } from "./explore/explore.component";
 import { TestomonialsComponent } from "./testomonials/testomonials.component";
 import { AchievementsComponent } from "./achievements/achievements.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [JumbotronComponent, HeaderComponent, BenefitCardsComponent, ExploreComponent, TestomonialsComponent, AchievementsComponent],
+  imports: [JumbotronComponent, HeaderComponent, BenefitCardsComponent, ExploreComponent, TestomonialsComponent, AchievementsComponent, FooterComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css',
   encapsulation: ViewEncapsulation.None
