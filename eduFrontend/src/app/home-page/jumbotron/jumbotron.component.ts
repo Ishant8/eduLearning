06fb8fbd5957bbb6aed42efcd623d4055e38df0b
Anant Patel <anantpatel288@gmail.com>
2024-10-22 14:47:13 +0530

    Jumbotron changed for courses table

--- eduFrontend/src/app/home-page/jumbotron/jumbotron.component.ts
+++ eduFrontend/src/app/home-page/jumbotron/jumbotron.component.ts
@@ -1,4 +1,4 @@
-import { Component, computed, inject } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ProfileService } from '../../profile-page/profile.service';
 
@@ -13,4 +13,7 @@ export class JumbotronComponent {
   private profileService = inject(ProfileService);
 
   userDet = computed(()=>this.profileService.profile());
+  title = input.required()
+  description = input.required()
+  buttonType = input()
 }
